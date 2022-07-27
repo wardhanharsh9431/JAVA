b772182a8be3b61b3d7b44627b64fa37819026ff
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-07-27 22:49:55 +0530

    Add files via upload

---
+++ main7.java
@@ -0,0 +1,25 @@
+// Using Methods in JAVA
+public class main7 {
+    static int logic(int x, int y) {
+        int z;
+        if (x > y) {
+            z = x + y;
+        } else {
+            z = (x + y) * 5;
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
+        c = logic(a, b);
+        int a1 = 1;
+        int b1 = 2;
+        int c1;
+        c1 = logic(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
+    }
+}
