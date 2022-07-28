994d0be330428ed1730b3bc2131fd3c1e1b72493
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-07-28 20:17:39 +0530

    Add files via upload

---
+++ main10.java
@@ -0,0 +1,16 @@
+// to print * using static method in given order
+public class main10 {
+    static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        pattern(12);
+    }
+
+}
