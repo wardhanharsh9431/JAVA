a9eb10199fc124429579c0ce0bbbc3081ab3692f
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-08-03 20:55:37 +0530

    Add files via upload

---
+++ main21.java
@@ -0,0 +1,35 @@
+
+class Cylinder {
+    private int radius;
+    private int height;
+
+    public int getRadius() {
+        return radius;
+
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+}
+
+public class main21 {
+    public static void main(String[] args) {
+        Cylinder myCylinder = new Cylinder();
+        myCylinder.setHeight(12);
+        int h = myCylinder.getHeight();
+        System.out.println(myCylinder.getHeight());
+        myCylinder.setRadius(9);
+        int r = myCylinder.getRadius();
+        System.out.println(myCylinder.getRadius());
+    }
+}
