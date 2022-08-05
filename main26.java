adae46c118311821dc39fcb4ecba5dd401f569a0
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-08-05 21:29:44 +0530

    Add files via upload

---
+++ main26.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class main26 {
+    public static void main(String[] args) {
+        // specify L,B and H
+        double L = 30, B = 5, H = 10;
+
+        // calculate SurfaceArea
+        double SurfaceArea = 2 * (L * B + B * H + H * L);
+
+        // calculate Volume
+        double Volume = L * B * H;
+
+        System.out.println(
+                "The Surface area of cuboid is : "
+                        + SurfaceArea);
+
+        System.out.println("The Volume of the cuboid is : "
+                + Volume);
+    }
+}
