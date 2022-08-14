891e4ff388decf5e21bb60e8e4d549860d0295c6
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-08-14 21:31:26 +0530

    Add files via upload

---
+++ main36.java
@@ -0,0 +1,26 @@
+class main36 {
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+
+class Dog extends main36 {
+    void bark() {
+        System.out.println("barking...");
+    }
+}
+
+class BabyDog extends Dog {
+    void weep() {
+        System.out.println("weeping...");
+    }
+}
+
+class TestInheritance2 {
+    public static void main(String args[]) {
+        BabyDog d = new BabyDog();
+        d.weep();
+        d.bark();
+        d.eat();
+    }
+}
