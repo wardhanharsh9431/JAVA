f9851e34d265e039659e52d74f94b2d158b48c5c
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-09-08 18:07:05 +0530

    Add files via upload

---
+++ main64.java
@@ -0,0 +1,20 @@
+package com.scien;
+
+import com.tech.A;
+
+class B {
+    void m3() {
+        System.out.println("Hello Java");
+    }
+
+public static void main(String[] args)
+ { 
+   A a = new A(); 
+   a.m1(); 
+
+   A a1 = new A(); 
+    a1.m2; 
+   B b = new B(); 
+    b.m3(); 
+  }
+}
