a00ec143d4f55d675980ac23965a05bdee44da14
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-09-14 21:25:02 +0530

    Add files via upload

---
+++ any.java
@@ -0,0 +1,9 @@
+public class any {
+    public static void main(String[] args) {
+        int n1 = 225;
+        int n2 = 115;
+        int sum = 0;
+        sum = n1 + n2;
+        System.out.println("The sum of nuumbers is" + sum);
+    }
+}
