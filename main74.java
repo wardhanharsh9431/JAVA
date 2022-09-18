c9c0c02cb02d393fabba073b8d0bae7186ced18d
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-09-18 16:04:52 +0530

    Add files via upload

---
+++ main74.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class main74 {
+    public static void printMyName(String name) {
+
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String ags[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);
+    }
+
+}
