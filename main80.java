805491884ca2f65b0d0957052e52827bc43a178e
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-09-23 23:00:29 +0530

    Add files via upload

---
+++ main80.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class main80 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No. of rows");
+        int n = sc.nextInt();
+        System.out.println("Enter No. of colomns");
+        int m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
