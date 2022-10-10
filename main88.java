82a15dfa69fb19b9a6a5f5388ec5c0973362cfbc
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-10-10 22:10:52 +0530

    Add files via upload

---
+++ main88.java
@@ -0,0 +1,30 @@
+class MyEmploye {
+    private int id;
+    private String name;
+
+    public String getname() {
+        return name;
+    }
+
+    public void setname(String n) {
+        name = n;
+    }
+
+    public void setid(int i) {
+        id = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+public class main88 {
+    public static void main(String[] args) {
+        MyEmploye harry = new MyEmploye();
+        harry.setname("CodeWithHarry");
+        System.out.println(harry.getname());
+        harry.setid(234);
+        System.out.println(harry.getId());
+    }
+}
