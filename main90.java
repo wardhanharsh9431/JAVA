707f137269530465cbfd0ba1142c84e5c979c902
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-10-12 20:37:47 +0530

    Add files via upload

---
+++ main90.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class main90 {
+    public static void main(String[] args) {
+        int[] marks = new int[5];
+        marks[0] = 99;// Physics
+        marks[1] = 98;// Chemistry
+        marks[2] = 91;// Mathematics
+        marks[3] = 90;// English
+        marks[4] = 95;// Physical Education;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+    }
+}
