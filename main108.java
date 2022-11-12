21fcef9acd7c8d84798f247ae58104bfff0edf47
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-11-12 23:17:56 +0530

    Add files via upload

---
+++ main108.java
@@ -0,0 +1,24 @@
+// Java Program to Find the Volume and Surface Area of
+// Cuboids
+
+import java.io.*;
+
+class main108 {
+    public static void main(String[] args) {
+        // specify L,B and H
+        double L = 20, B = 15, H = 10;
+
+        // calculate SurfaceArea
+        double SurfaceArea = 2 * (L * B + B * H + H * L);
+
+        // calculate Volume
+        double Volume = L * B * H;
+
+        System.out.println(
+                "The Surface area of cuboid is : "
+                        + SurfaceArea);
+
+        System.out.println("The Volume of the cuboid is : "
+                + Volume);
+    }
+}
