7688025e8539a3e14f4e3e4c91ec1a98b05ae210
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-11-16 22:03:25 +0530

    Add files via upload

---
+++ main111.java
@@ -0,0 +1,28 @@
+// Java program to demonstrate the various
+// methods to iterate over a string array 
+
+public class main111 {
+    public static void main(String[] args) {
+        String[] arr = { "Apple", "Banana", "Orange" };
+
+        // First method
+        for (String i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        // Second method
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Third method
+        int i = 0;
+        while (i < arr.length) {
+            System.out.print(arr[i] + " ");
+            i++;
+        }
+        System.out.println();
+    }
+}
