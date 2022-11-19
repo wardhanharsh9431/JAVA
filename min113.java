caece78dc4bf598a45430f228139bd30f6bef195
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-11-19 12:39:11 +0530

    Add files via upload

---
+++ min113.java
@@ -0,0 +1,26 @@
+class min113 {
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+
+class Dog extends main37 {
+    void bark() {
+        System.out.println("barking...");
+    }
+}
+
+class Cat extends main37 {
+    void meow() {
+        System.out.println("meowing...");
+    }
+}
+
+class TestInheritance3 {
+    public static void main(String args[]) {
+        Cat c = new Cat();
+        c.meow();
+        c.eat();
+        // c.bark();//C.T.Error
+    }
+}
