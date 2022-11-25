b404fdb96ae694e0f4319642b7edfa72fb503486
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-11-25 23:52:13 +0530

    Add files via upload

---
+++ main119.java
@@ -0,0 +1,20 @@
+public class main119 {
+    public static void main(String[] args) {
+        String[] strArray = { "Ani", "Sam", "Joe" };
+        boolean x = false; // initializing x to false
+        int in = 0; // declaration of index variable
+        String s = "Sam"; // String to be searched
+        // Iteration of the String Array
+        for (int i = 0; i < strArray.length; i++) {
+            if (s.equals(strArray[i])) {
+                in = i;
+                x = true;
+                break;
+            }
+        }
+        if (x)
+            System.out.println(s + " String is found at index " + in);
+        else
+            System.out.println(s + " String is not found in the array");
+    }
+}
