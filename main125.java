2b68ea857aafb1d8a570cc7683dba8ae51c09d1a
Harsh Wardhan <109651541+wardhanharsh9431@users.noreply.github.com>
2022-12-01 22:10:54 +0530

    Add files via upload

---
+++ main125.java
@@ -0,0 +1,23 @@
+public class main125 {
+
+    public static void main(String[] args) {
+        boolean flag = palindromeCheck("sitonapanotis");
+        System.out.println(flag);
+        flag = palindromeCheck("nine");
+        System.out.println(flag);
+        flag = palindromeCheck("amanaplanacanalpanama");
+        System.out.println(flag);
+
+    }
+
+    /* Recursive Java example to check for palindromes */
+    public static boolean palindromeCheck(String s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return true;
+        }
+        if (s.charAt(0) == s.charAt(s.length() - 1)) {
+            return palindromeCheck(s.substring(1, s.length() - 1));
+        }
+        return false;
+    }
+}
